29db848b6447969cdd9ffa581a3dcb33e415daf6
Caio André <caioandresgit@outlook.com>
2024-12-02 20:53:41 -0300

    refactor(AST): ASTNode id counter created.

--- src/frontend/ast-nodes.ts
+++ src/frontend/ast-nodes.ts
@@ -2,3 +2,11 @@ interface ASTNode {
   id: number;
   type: string;
 }
+
+class ASTNodeCounter {
+  private static currentId: number = 0;
+
+  public static getNextId(): number {
+    return ++this.currentId;
+  }
+}
