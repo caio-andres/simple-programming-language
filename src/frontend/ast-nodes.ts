d19815c18c1ff3844a98871056ce6a9516d5e17e
Caio André <caioandresgit@outlook.com>
2024-12-02 21:25:27 -0300

    refactor(AST): Number, name and assignment Node implemented.

--- src/frontend/ast-nodes.ts
+++ src/frontend/ast-nodes.ts
@@ -10,3 +10,27 @@ class ASTNodeCounter {
     return ++this.currentId;
   }
 }
+
+class NumberNode implements ASTNode {
+  id: number;
+  constructor(public value: string) {
+    this.id = ASTNodeCounter.getNextId();
+  }
+  type = "Number";
+}
+
+class NameNode implements ASTNode {
+  id: number;
+  constructor(public value: string) {
+    this.id = ASTNodeCounter.getNextId();
+  }
+  type = "Name";
+}
+
+class AssignmentNode implements ASTNode {
+  id: number;
+  constructor(public name: NameNode, public value: ASTNode) {
+    this.id = ASTNodeCounter.getNextId();
+  }
+  type = "Assignment";
+}
