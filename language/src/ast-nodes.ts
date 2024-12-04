e688ce8e207e7b6f87f94a03b090c63cc9070909
Caio André <caioandresgit@outlook.com>
2024-12-04 16:32:03 -0300

    release/refactor(interface / programming language): Interface created with react typescript vite / Programming language files moved to 'language' folder as new directory

---
+++ language/src/ast-nodes.ts
@@ -0,0 +1,72 @@
+export interface ASTNode {
+  id: number;
+  type: string;
+}
+
+export class ASTNodeCounter {
+  private static currentId: number = 0;
+
+  public static getNextId(): number {
+    return ++this.currentId;
+  }
+}
+
+export class NumberNode implements ASTNode {
+  id: number;
+  constructor(public value: string) {
+    this.id = ASTNodeCounter.getNextId();
+  }
+  type = "Number";
+}
+
+export class NameNode implements ASTNode {
+  id: number;
+  constructor(public value: string) {
+    this.id = ASTNodeCounter.getNextId();
+  }
+  type = "Name";
+}
+
+export class AssignmentNode implements ASTNode {
+  id: number;
+  constructor(public name: NameNode, public value: ASTNode) {
+    this.id = ASTNodeCounter.getNextId();
+  }
+  type = "Assignment";
+}
+
+export class BinaryOpNode implements ASTNode {
+  id: number;
+  constructor(
+    public left: ASTNode,
+    public operator: string,
+    public right: ASTNode
+  ) {
+    this.id = ASTNodeCounter.getNextId();
+  }
+  type = "BinaryOp";
+}
+
+export class ConditionalNode implements ASTNode {
+  id: number;
+  constructor(
+    public left: ASTNode,
+    public operator: string,
+    public right: ASTNode
+  ) {
+    this.id = ASTNodeCounter.getNextId();
+  }
+  type = "Conditional";
+}
+
+export class IfNode implements ASTNode {
+  id: number;
+  constructor(
+    public condition: ASTNode,
+    public thenBranch: ASTNode,
+    public elseBranch: ASTNode | null = null
+  ) {
+    this.id = ASTNodeCounter.getNextId();
+  }
+  type = "If";
+}
